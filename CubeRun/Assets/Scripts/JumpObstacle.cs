226a9aaf9064e57599a4fe28dc7502aae5ef8932
Lhotakry <lhota.krystof@gmail.com>
2021-12-24 11:28:20 +0100

    cuberun

---
+++ CubeRun/Assets/Scripts/JumpObstacle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class JumpObstacle : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float JumpForce = 500f;
+    void OnCollisionEnter (Collision collisionInfo){
+        if (collisionInfo.collider.tag == "JumpObstacle"){
+            rb.AddForce(0, JumpForce, 0);
+        }
+    }
+
+
+}
