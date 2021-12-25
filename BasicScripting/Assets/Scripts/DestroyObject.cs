c652d2065a208fef3d6a677e5049ad73c5986ee2
Lhotakry <lhota.krystof@gmail.com>
2021-12-25 22:37:48 +0100

    Chtel jsem si prozkouset zakladni scripty a nakonec mi z toho zacala vznikat mala hra

---
+++ BasicScripting/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyObject : MonoBehaviour
+{   
+    public NextLevel nextlevel;
+    public GameObject WinText;
+    public GameObject TryItAgain;
+    public GameObject Instruction;
+    public GameObject Sphere;
+    public float delay = 2f;
+    Vector3 originalPosSphere;
+    Vector3 originalPosPlayer;
+    public GameObject Player;
+    
+    public void Start()
+    {
+        originalPosSphere = Sphere.transform.position;
+        originalPosPlayer = Player.transform.position;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "Player")
+        {   
+            Instruction.SetActive(false);
+            WinText.SetActive(true);
+            Invoke("NextLevel", delay);
+            Sphere.SetActive(false);
+
+        }
+        if(collision.gameObject.tag == "Ground")
+        {
+            Instruction.SetActive(false);
+            TryItAgain.SetActive(true);
+            Invoke("Restart", delay);
+        }
+    }
+    void Restart()
+    {
+        Sphere.transform.position = originalPosSphere;
+        Player.transform.position = originalPosPlayer;
+        TryItAgain.SetActive(false);
+    }
+
+    void NextLevel()
+    {
+        nextlevel.enabled = true;
+    }
+}
---
+++ BasicScripting/Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject Sphere;
+    public GameObject Instruction;
+    public Movement movement;
+    void Update()
+    {
+        if(Input.GetKey("p"))
+        {
+        Sphere.SetActive(true);
+        Instruction.SetActive(false);
+        movement.enabled = true;
+        }
+    }
+}
